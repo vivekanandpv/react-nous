225686db40aee309c76b6385500933526bb54df0
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-06 16:01:10 +0530

    done

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Report = (props) => {
+  const { year } = useParams();
+
+  useEffect(() => {
+    if (!+year) {
+      alert('invalid year!');
+    }
+  }, []);
+
   return (
     <>
       <h3>Report Component</h3>
