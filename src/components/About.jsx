657ac66ca96fe55d0c97cd97a3a82144b349e7ae
Vivekanand P V <vivekanandpv@outlook.com>
2022-07-07 14:22:28 +0530

    done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 const About = (props) => {
-  return (
-    <>
-      <h3>About Component</h3>
-    </>
-  );
+    const history = useHistory();
+
+    const navigate = () => {
+        history.push({
+            pathname: '/contact',
+            hash: 'fragment',
+            search: '?key=val&q=a'
+        });
+    }
+    return (
+        <>
+            <h3>About Component</h3>
+            <button className="btn btn-primary" onClick={navigate}>To Contact</button>
+        </>
+    );
 };
 
 export default About;
